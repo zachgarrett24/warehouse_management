73e29432e4e6bbd0c827e21a8aaa470d90853ec3
Zach Garrett <zacharytheodoregarrett@gmail.com>
2021-01-29 22:17:06 -0800

    update createTables

--- db/index.js
+++ db/index.js
@@ -4,15 +4,31 @@ const { Client } = require('pg'); // imports the pg module
 const client = new Client('postgres://localhost:5432/warehouse_management');
 
 async function getAllUsers() {
-    const { rows } = await client.query(
-      `SELECT id, username 
-      FROM users;
+    const { rows } = await client.query(`
+        SELECT *
+        FROM users;
     `);
   
     return rows;
 }
 
+async function createUser({ username, password, name, company }) {
+    try {
+      const { rows } = await client.query(`
+        INSERT INTO users(username, password, name, company)
+        VALUES ($1, $2, $3, $4)
+        ON CONFLICT (username) DO NOTHING
+        RETURNING *;
+      `, [username, password, name, company]);
+  
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+}
+
 module.exports = {
   client,
+  createUser,
   getAllUsers
 }
--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,7 @@
 // grab our client with destructuring from the export in index.js
 const { 
     client,
+    createUser,
     getAllUsers
 } = require('./index');
 
@@ -9,7 +10,7 @@ async function dropTables() {
         console.log('starting to drop tables...')
         await client.query(`
             DROP TABLE IF EXISTS users;
-        `)
+        `);
         console.log('finished dropping tables...')
     } catch (error) {
         console.error('error dropping tables!')
@@ -24,7 +25,10 @@ async function createTables() {
             CREATE TABLE users (
                 id SERIAL PRIMARY KEY,
                 username VARCHAR(255) UNIQUE NOT NULL,
-                password VARCHAR(255) NOT NULL
+                password VARCHAR(255) NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                company VARCHAR(255) NOT NULL,
+                active BOOLEAN DEFAULT true
             );
         `);
         console.log('finished building tables...')
@@ -34,11 +38,25 @@ async function createTables() {
     }
 }
 
+async function createInitialUsers() {
+    try {
+        console.log('starting to create users...');
+        const albert = await createUser({ username: 'albert', password: 'bertie99', name: 'Bert', company: 'House of Reptiles' });
+        const sandra = await createUser({ username: 'sandra', password: 'sosandy', name: 'Sandy Cheeks', company: 'Best Beach Towels' });
+        const glamgal = await createUser({ username: 'glamgal', password: 'soglam', name: 'Edward', company: 'Make-up Town' });
+        console.log('finished creating users!');
+    } catch (error) {
+        throw error;
+    }
+    
+}
+
 async function rebuildDB() {
     try {
         client.connect();
         await dropTables();
         await createTables();
+        await createInitialUsers();
     } catch (error) {
         throw error;
     }
