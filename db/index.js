375c8ba265a72062cd90a4e17781773065b7b40c
Zach Garrett <zacharytheodoregarrett@gmail.com>
2021-01-29 17:09:42 -0800

    first table seed

--- db/index.js
+++ db/index.js
@@ -3,6 +3,16 @@ const { Client } = require('pg'); // imports the pg module
 // supply the db name and location of the database
 const client = new Client('postgres://localhost:5432/warehouse_management');
 
+async function getAllUsers() {
+    const { rows } = await client.query(
+      `SELECT id, username 
+      FROM users;
+    `);
+  
+    return rows;
+}
+
 module.exports = {
   client,
+  getAllUsers
 }
--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,60 @@
 // grab our client with destructuring from the export in index.js
-const { client } = require('./index');
+const { 
+    client,
+    getAllUsers
+} = require('./index');
+
+async function dropTables() {
+    try {
+        console.log('starting to drop tables...')
+        await client.query(`
+            DROP TABLE IF EXISTS users;
+        `)
+        console.log('finished dropping tables...')
+    } catch (error) {
+        console.error('error dropping tables!')
+        throw error;
+    }
+}
+
+async function createTables() {
+    try {
+        console.log('starting to build tables...')
+        await client.query(`
+            CREATE TABLE users (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(255) UNIQUE NOT NULL,
+                password VARCHAR(255) NOT NULL
+            );
+        `);
+        console.log('finished building tables...')
+    } catch (error) {
+        console.log('error building tables!')
+        throw error;
+    }
+}
+
+async function rebuildDB() {
+    try {
+        client.connect();
+        await dropTables();
+        await createTables();
+    } catch (error) {
+        throw error;
+    }
+}
 
 async function testDB() {
   try {
-    // connect the client to the database, finally
-    client.connect();
+    const users = await getAllUsers();
+    console.log(users);
 
-    const { rows } = await client.query(`SELECT * FROM users;`);
-    console.log(rows);
-    
   } catch (error) {
     console.error(error);
-  } finally {
-    // it's important to close out the client connection
-    client.end();
-  }
+  } 
 }
 
-testDB();
+rebuildDB()
+    .then(testDB)
+    .catch(console.error)
+    .finally(() => client.end());
